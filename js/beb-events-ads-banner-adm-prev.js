708c12461ce44b3766d23b811a1634247f14f1e6
fabiettos <fabiettos@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-02 12:33:17 +0000

    correct version file declaration
    git-svn-id: https://plugins.svn.wordpress.org/eventsads-banner/trunk@1126277 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/beb-events-ads-banner-adm-prev.js
@@ -0,0 +1,40 @@
+jQuery(document).ready(function($){
+	/*
+	var beb_eab_url_get = window.location.href;
+	var beb_eab_pagina = beb_eab_url_get.split ('?');
+	beb_eab_pagina = beb_eab_pagina[1].split ('=');
+	beb_eab_pagina = beb_eab_pagina[1];
+	*/
+	var beb_eab_url_tmp = $("#beb-eab-banner-bottone").attr("src");
+	beb_eab_url_tmp = beb_eab_url_tmp.split('/');
+	delete beb_eab_url_tmp [0];
+	delete beb_eab_url_tmp [beb_eab_url_tmp.length-1];
+	var beb_eab_url = beb_eab_url_tmp.join('/');
+	beb_eab_url = 'http:' + beb_eab_url;
+	
+	$('.beb-eab-stato-banner-riassunto').each(function(index, element){
+		if ($(this).children('input[name="beb_eab_banner_preview[stato_banner]"]').val() == 'on') {
+			$(this).css('background-image', 'url(' + beb_eab_url + 'on.png)').mouseover(function(){
+				$(this).css('background-image', 'url(' + beb_eab_url + 'on_off.png)');
+			}).mouseout(function() {
+				$(this).css('background-image', 'url(' + beb_eab_url + 'on.png)');
+			});
+			$(this).click(function(){
+				$(this).children('input[name="beb_eab_banner_preview[stato_banner]"]').val('off');
+			});
+		}
+		if ($(this).children('input[name="beb_eab_banner_preview[stato_banner]"]').val() == 'off') {
+			$(this).css('background-image', 'url(' + beb_eab_url + 'off.png)').mouseover(function(){
+				$(this).css('background-image', 'url(' + beb_eab_url + 'on_off.png)');
+			}).mouseout(function() {
+				$(this).css('background-image', 'url(' + beb_eab_url + 'off.png)');
+			});
+			$(this).click(function(){
+				$(this).children('input[name="beb_eab_banner_preview[stato_banner]"]').val('on');
+			});
+		}
+	});
+	$('#beb-eab-banner-remove').click(function(){
+		$('input[name="beb_eab_banner_preview[remove_banner]"]').val('si');
+	});
+});
